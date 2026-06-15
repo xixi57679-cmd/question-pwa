84d8eb13a38c3402438735e4dcacb79c8e889d17
xixi57679-cmd <xixi57679@gmail.com>
2026-06-15 23:58:19 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = "question-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./src/app.js",
+  "./src/styles.css",
+  "./src/data/questions.ts",
+  "./manifest.webmanifest",
+  "./public/icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
